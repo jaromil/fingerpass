7b96485051aefbcde047855728d8c25eb35d98da
Jaromil <jaromil@dyne.org>
2018-08-30 17:09:04 +0200

    implemented hamming difference calculation

--- fingerpass.c
+++ fingerpass.c
@@ -186,6 +186,40 @@ struct fp_dscv_dev *discover_device(struct fp_dscv_dev **discovered_devs)
 	return ddev;
 }
 
+int popcount64b(uint64_t x) {
+    //types and constants
+	const uint64_t m1  = 0x5555555555555555; //binary: 0101...
+	const uint64_t m2  = 0x3333333333333333; //binary: 00110011..
+	const uint64_t m4  = 0x0f0f0f0f0f0f0f0f; //binary:  4 zeros,  4 ones ...
+	const uint64_t m8  = 0x00ff00ff00ff00ff; //binary:  8 zeros,  8 ones ...
+	const uint64_t m16 = 0x0000ffff0000ffff; //binary: 16 zeros, 16 ones ...
+	const uint64_t m32 = 0x00000000ffffffff; //binary: 32 zeros, 32 ones
+	const uint64_t hff = 0xffffffffffffffff; //binary: all ones
+	const uint64_t h01 = 0x0101010101010101; //the sum of 256 to the power of 0,1,2,3...
+	x -= (x >> 1) & m1;             //put count of each 2 bits into those 2 bits
+	x = (x & m2) + ((x >> 2) & m2); //put count of each 4 bits into those 4 bits 
+	x = (x + (x >> 4)) & m4;        //put count of each 8 bits into those 8 bits 
+	x += x >>  8;  //put count of each 16 bits into their lowest 8 bits
+	x += x >> 16;  //put count of each 32 bits into their lowest 8 bits
+	x += x >> 32;  //put count of each 64 bits into their lowest 8 bits
+	return x & 0x7f;
+}
+
+// compare bit by bit two arrays and returns the hamming distance
+int hamming_distance(int8_t *left, int8_t *right, size_t len) {
+	int distance, c, nlen;
+	int8_t xor;
+	uint64_t *l, *r;
+	nlen = len/3; // 64bit chunks
+	distance = 0;
+	l=(uint64_t*)left;
+	r=(uint64_t*)right;
+	for(c=0;c<nlen;c++,l++,r++) {
+		distance += popcount64b(  l[c] ^ r[c] );
+	}
+	return distance;
+}
+
 // allocates a new array, needs freeing
 int8_t *minutiae_feature_vector(int8_t *feature_vector,
                                 fp_minutia **nist_minutiae, unsigned int nmin)  {
@@ -433,8 +467,7 @@ int main(int argc, char **argv) {
 			if(i==j) {
 				fprintf(stderr,"CMP i:%i j:%i\n",i,j);
 				continue; }
-			eq = memcmp(*l,*r,featurebytes); // TODO: constant time cmp
-			// hamming
+			eq = hamming_distance(*l,*r,featurebytes);
 			fprintf(stderr,"CMP i:%i j:%i = %i\n",i,j,eq);
 		}
 	}
