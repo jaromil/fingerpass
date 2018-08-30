b28021ac21f82ff644d652a9f0058f1c9be21c0d
Jaromil <jaromil@dyne.org>
2018-08-30 08:59:06 +0200

    implementation of VPij distance calculation
    started implementing quantization

--- fingerpass.c
+++ fingerpass.c
@@ -24,8 +24,12 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <inttypes.h>
 
 #include <libfprint/fprint.h>
+
+#define fivebit 0x1f // bitmask 00011111
 
 // Copieed from mintct in NBIS 5.0 - may introduce binary
 // incompatibility if not aligned to exact library version.
@@ -44,6 +48,23 @@ typedef struct {
 	int num_nbrs;
 } fp_minutia;
 
+typedef struct {
+	int x;
+	int y;
+	int t;
+} XYT;
+
+typedef struct {
+	float L;
+	float alpha;
+	float beta;
+} VPij;
+
+typedef struct {
+//	XYT i, j;
+//	VPij delta;
+	int16_t quantized;
+} QMin;
 
 /* This is the number of integer directions to be used in semicircle. */
 /* CAUTION: If NUM_DIRECTIONS is changed, then the following will     */
@@ -52,54 +73,64 @@ typedef struct {
 /*                             FORK_INTERVAL                          */
 #define NUM_DIRECTIONS          16
 
+#define max(a, b)   ((a) > (b) ? (a) : (b))
+#define min(a, b)   ((a) < (b) ? (a) : (b))
 #define sround(x) ((int) (((x)<0) ? (x)-0.5 : (x)+0.5))
 
-/*************************************************************************
-**************************************************************************
-#cat: minutia_XYT - Converts XYT minutiae attributes in LFS native
-#cat:        representation to NIST internal representation
+/* minutia_XYT - Converts XYT minutiae attributes in LFS native
+        representation to NIST internal representation
+
    Input:
       minutia  - LFS minutia structure containing attributes to be converted
    Output:
       ox       - NIST internal based x-pixel coordinate
       oy       - NIST internal based y-pixel coordinate
       ot       - NIST internal based minutia direction/orientation
-**************************************************************************/
-/*       XYT's according to NIST internal rep:           */
-/*      1. pixel coordinates with origin bottom-left    */
-/*       2. orientation in degrees on range [0..360]     */
-/*          with 0 pointing east and increasing counter  */
-/*          clockwise (same as M1)                       */
-/*       3. direction pointing out and away from the     */
-/*             ridge ending or bifurcation valley        */
-/*             (opposite direction from M1)              */
-void minutia_XYT(int *ox, int *oy, int *ot,
-                          fp_minutia *minutia) {
-	int x, y, t;
+*/
+     //  XYT's according to NIST internal rep:
+     // 1. pixel coordinates with origin bottom-left
+     //  2. orientation in degrees on range [0..360]
+     //     with 0 pointing east and increasing counter
+     //     clockwise (same as M1)
+     //  3. direction pointing out and away from the
+     //        ridge ending or bifurcation valley
+     //        (opposite direction from M1)
+XYT *minutia_XYT(XYT *coord, fp_minutia *minutia) {
 	float degrees_per_unit;
+	int t;
+	     //  XYT's according to NIST internal rep:
+	     // 1. pixel coordinates with origin bottom-left
+	     //  2. orientation in degrees on range [0..360]
+	     //     with 0 pointing east and increasing counter
+	     //     clockwise (same as M1)
+	     //  3. direction pointing out and away from the
+	     //        ridge ending or bifurcation valley
+	     //        (opposite direction from M1)
 
-	/*       XYT's according to NIST internal rep:           */
-	/*      1. pixel coordinates with origin bottom-left    */
-	/*       2. orientation in degrees on range [0..360]     */
-	/*          with 0 pointing east and increasing counter  */
-	/*          clockwise (same as M1)                       */
-	/*       3. direction pointing out and away from the     */
-	/*             ridge ending or bifurcation valley        */
-	/*             (opposite direction from M1)              */
-
-	x = minutia->x;
-	y = minutia->y;
+    coord->x = minutia->x;
+	coord->y = minutia->y;
 
 	degrees_per_unit = 180 / (float)NUM_DIRECTIONS;
-
+	
 	t = (270 - sround(minutia->direction * degrees_per_unit)) % 360;
-	if(t < 0){
-		t += 360;
-	}
+	if(t < 0) t += 360;
+	coord->t = t;
+	return coord;
+}
 
-	*ox = x;
-	*oy = y;
-	*ot = t;
+VPij *XYT_distance(VPij *distance, XYT *i, XYT *j) {
+	double X, Y;
+	X = (j->x - i->x)*cos(i->t) - (j->y - i->y)*sin(i->t);
+	Y = (j->x - i->x)*sin(i->t) - (j->y - i->y)*cos(i->t);
+	distance->L     = sqrt( pow(X,2) + pow(Y,2) );
+	distance->alpha = atan(Y/X);
+	distance->beta  = distance->alpha + j->t - i->t;
+	return distance;
+}
+
+uint16_t distance_quantize(QMin *minutia, VPij *delta) {
+	uint16_t bits;
+	bits = 0x0;
 }
 
 struct fp_dscv_dev *discover_device(struct fp_dscv_dev **discovered_devs)
@@ -164,29 +195,39 @@ int main(void)
 		goto out_close;
 	}
 
-	// full standardization
-	fp_img_standardize(img);
-	fp_img_standardize(img);
+	// standardization
 	fp_img_standardize(img);
 
 	// extract XYT minutiae points
-	fp_minutia **minutiae;
+	fp_minutia **nist_minutiae;
 	int nmin;
-	minutiae = (fp_minutia**)fp_img_get_minutiae(img, &nmin);
-	int i, x, y, t;
+	nist_minutiae = (fp_minutia**)fp_img_get_minutiae(img, &nmin);
+	fprintf(stderr,"%i minutiae extracted from scanned fingerprint\n",nmin);
+	QMin *minutiae = calloc(nmin,sizeof(QMin));
+	XYT icoord;
+	XYT jcoord;
+	unsigned int i, j;
+	VPij delta;
 	for(i=0; i<nmin; i++) {
-		minutia_XYT(&x, &y, &t,
-		            minutiae[i]);
-		fprintf(stdout,"x:%i \t y:%i \t t:%i\n",x, y, t);
-	}
+		// fprintf(stdout,"I: x:%i \t y:%i \t t:%i\n",icoord.x, icoord.y, icoord.t);
+		for(j=0; j<nmin; j++) {
+			// fprintf(stdout,"J: x:%i \t y:%i \t t:%i\n",jcoord.x, jcoord.y, jcoord.t);
 
+			if(i==j) continue;
+			XYT_distance( &delta,
+			              minutia_XYT(&icoord, nist_minutiae[i]),
+			              minutia_XYT(&jcoord, nist_minutiae[j]) );
+			fprintf(stdout,"I:%u \t J:%u \t-\t L:%f \t alpha:%f \t beta:%f\n",
+			        i,j, delta.L, delta.alpha, delta.beta);
+		}
+	}
 	// r = fp_img_save_to_file(img, "finger.pgm");
 	// if (r) {
 	// 	fprintf(stderr, "img save failed, code %d\n", r);
 	// 	goto out_close;
 	// }
 	fp_img_free(img);
-
+	free(minutiae);
 	r = 0;
 out_close:
 	fp_dev_close(dev);
